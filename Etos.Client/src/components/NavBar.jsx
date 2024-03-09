ca9e9eb442f151f343b563bea36498e5f0decad2
noctvrno <feed.faceit@gmail.com>
2024-03-09 02:05:17 +0200

    Add draft NavBar.

---
+++ Etos.Client/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import './NavBar.css';
+
+function NavBar() {
+    return <div className="nav-bar">
+        <h2>Test1</h2>
+        <h2>Test2</h2>
+        <h2>Test3</h2>
+        <h2>Test4</h2>
+    </div>;
+}
+
+export default NavBar;
--- etos.client/src/App.jsx
+++ etos.client/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
-    return <h1>Hello World!</h1>;
+    return <NavBar/>;
 }
 
 export default App;
