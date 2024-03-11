36d48a080e7fa94fa09b45bcf027ecdbc167cc49
noctvrno <feed.faceit@gmail.com>
2024-03-11 23:12:39 +0200

    Add Material UI project reference. Use mui/ListBox for the NavBar.

--- Etos.Client/src/components/NavBar.jsx
+++ Etos.Client/src/components/NavBar.jsx
@@ -1,12 +1,22 @@
 import './NavBar.css';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
 
-function NavBar() {
-    return <div className="nav-bar">
-        <h2>Test1</h2>
-        <h2>Test2</h2>
-        <h2>Test3</h2>
-        <h2>Test4</h2>
-    </div>;
+export default function NavBar() {
+    return (
+        <List className='nav-bar'>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <HomeIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Home" />
+                </ListItemButton>
+            </ListItem>
+        </List>
+    );
 }
-
-export default NavBar;
