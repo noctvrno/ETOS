25d79a34dd174cf347a52fcaf22382a85ce4dd2d
noctvrno <feed.faceit@gmail.com>
2024-03-24 02:13:40 +0200

    Add ActivityFeed draft via Box.

---
+++ Etos.Client/src/components/ActivityFeed.jsx
@@ -0,0 +1,17 @@
+import { Stack, Typography } from "@mui/material";
+
+export default function ActivityFeed() {
+    return (
+        <Stack>
+            <Typography variant="h3">
+                Training
+            </Typography>
+            <Typography variant="h3">
+                Orientation
+            </Typography>
+            <Typography variant="h3">
+                Feedback
+            </Typography>
+        </Stack>
+    );
+}
