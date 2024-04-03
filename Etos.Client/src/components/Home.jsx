ae09f9fa81c2413db21b479c444435a6888a16e1
noctvrno <feed.faceit@gmail.com>
2024-04-04 02:15:13 +0300

    Remove Box prop from Box element.

--- Etos.Client/src/components/Home.jsx
+++ Etos.Client/src/components/Home.jsx
@@ -4,7 +4,7 @@ import ActivityFeed from "./ActivityFeed";
 
 export default function Home() {
     return (
-        <Box Box display="flex" height="100vh" width="100vw">
+        <Box display="flex" height="100vh" width="100vw">
             <Box>
                 <NavBar />
             </Box>
