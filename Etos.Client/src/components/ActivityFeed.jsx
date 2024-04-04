199c6b5746110cf6a438cc1bdf3f9db089d1786f
noctvrno <feed.faceit@gmail.com>
2024-04-04 23:57:08 +0300

    Activity feed test button

--- Etos.Client/src/components/ActivityFeed.jsx
+++ Etos.Client/src/components/ActivityFeed.jsx
@@ -1,8 +1,20 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 
 export default function ActivityFeed() {
     return (
         <Stack>
+            <Button
+                color="secondary"
+                variant="outlined"
+                style={{
+                    fontSize: 30,
+                    justifyContent: "flex-start",
+                    color: "white",
+                    margin: 20,
+                    borderRadius: 50
+                }}>
+                Test
+            </Button>
             <Typography variant="h3">
                 Training
             </Typography>
