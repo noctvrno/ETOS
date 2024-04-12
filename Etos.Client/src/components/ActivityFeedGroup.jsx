af5d4d4bbbb7c18780f7e543a51cda9070dcaaef
noctvrno <feed.faceit@gmail.com>
2024-04-12 23:39:35 +0300

    Introduce avatar to NavBar. Wrap Activity buttons in a Stack to conveniently provide spacing.

--- Etos.Client/src/components/ActivityFeedGroup.jsx
+++ Etos.Client/src/components/ActivityFeedGroup.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 
 const items = ["Test1", "Test2"];
 
@@ -9,30 +9,31 @@ export default function ActivityFeedGroup(props) {
             <Typography
                 variant="h4"
                 sx={{
-                    marginTop: "2rem",
+                    margin: "2rem",
                     marginLeft: "1rem"
                 }}>
                 {props.groupName}
             </Typography>
-            {items.map(itemName => {
-                return (
-                    <Button
-                        key={itemName}
-                        color="secondary"
-                        variant="outlined"
-                        style={{
-                            justifyContent: "flex-start",
-                            color: "white",
-                            marginTop: "1rem",
-                            marginLeft: "2rem",
-                            borderRadius: "50px",
-                            textTransform: "none",
-                            whiteSpace: 'normal'
-                        }}>
-                        <Typography variant='h5'>{itemName}</Typography>
-                    </Button>
-                )
-            })}
+            <Stack spacing={2.5}>
+                {items.map(itemName => {
+                    return (
+                        <Button
+                            key={itemName}
+                            color="secondary"
+                            variant="outlined"
+                            style={{
+                                justifyContent: "flex-start",
+                                color: "white",
+                                marginLeft: "2rem",
+                                borderRadius: "50px",
+                                textTransform: "none",
+                                whiteSpace: 'normal'
+                            }}>
+                            <Typography variant='h5'>{itemName}</Typography>
+                        </Button>
+                    )
+                })}
+            </Stack>
         </>
     );
 }
