159d68d8f61193ae8a488a92d6040ac0aaebedde
noctvrno <feed.faceit@gmail.com>
2024-05-12 22:17:04 +0300

    Add Mediatr NuGet package and configure it via AddApplication extension method. (#11)

--- Etos.Presentation.WebApi/Program.cs
+++ Etos.Presentation.WebApi/Program.cs
@@ -1,6 +1,11 @@
+using Etos.Application;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
