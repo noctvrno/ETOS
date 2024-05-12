b5211f5af0fca4d36b96f8b17b8732e98c9f3e71
noctvrno <feed.faceit@gmail.com>
2024-05-12 22:17:38 +0300

    Configure proxy for backend Api. (#11)

--- Etos.Presentation.Client/vite.config.js
+++ Etos.Presentation.Client/vite.config.js
@@ -30,9 +30,6 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5001';
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
@@ -43,8 +40,9 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '^/weatherforecast': {
-                target,
+            '/api': {
+                target: "http://localhost:5219",
+                changeOrigin: true,
                 secure: false
             }
         },
