33e82e2151a3ab6c169a807bb602510e22d5e0c0
noctvrno <feed.faceit@gmail.com>
2024-05-18 00:29:07 +0300

    Add IQuery and IQueryHandler abstractions. (#11)

---
+++ Etos.Application/Abstractions/IQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Etos.Application.Abstractions;
+
+internal interface IQuery<out TResponse> : IRequest<TResponse> { }
---
+++ Etos.Application/Abstractions/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Etos.Application.Abstractions;
+
+internal interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+
+}
