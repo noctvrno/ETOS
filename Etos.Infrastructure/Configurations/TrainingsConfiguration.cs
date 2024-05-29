8c0f8522508cdc2f4f93816ea680733d6078f7a0
noctvrno <feed.faceit@gmail.com>
2024-05-30 00:33:30 +0300

    Add TrainingsConfiguration. (#14)

---
+++ Etos.Infrastructure/Configurations/TrainingsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Etos.Domain.Entities;
+using Etos.Domain.Primitives;
+using Etos.Infrastructure.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Etos.Infrastructure.Configurations;
+
+internal sealed class TrainingsConfiguration : IEntityTypeConfiguration<TrainingActivity>
+{
+    public void Configure(EntityTypeBuilder<TrainingActivity> builder)
+    {
+        builder.ToTable(TablesNames.TRAININGS);
+
+        builder.HasKey(t => t.Id);
+
+        builder
+            .Property(t => t.Id)
+            .HasConversion(id => id.Value, dbId => new ActivityId(dbId));
+
+        builder.Property(t => t.Title);
+    }
+}
