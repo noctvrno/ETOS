19c11a9f00a9dc2404f496f40a810ffe22f70b38
noctvrno <feed.faceit@gmail.com>
2024-06-02 01:20:24 +0300

    Create User Entity. (#14)

---
+++ Etos.Domain/Entities/User.cs
@@ -0,0 +1,40 @@
+﻿namespace Etos.Domain.Entities;
+
+public class User
+{
+    public UserId Id { get; }
+    public string FirstName { get;}
+    public string LastName { get; }
+    public string Email { get;}
+
+    private User(
+        UserId userId,
+        string firstName,
+        string lastName,
+        string email)
+    {
+        Id = userId;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public User()
+    {
+
+    }
+
+    public static User Create(
+        string firstName,
+        string lastName,
+        string email)
+    {
+        return new User(
+            new UserId(Guid.NewGuid()),
+            firstName,
+            lastName,
+            email);
+    }
+}
+
+public sealed record UserId(Guid Value);
