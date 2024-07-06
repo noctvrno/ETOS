c55e0185833d3603dbd8882aef23afe57595e3c5
noctvrno <feed.faceit@gmail.com>
2024-07-06 23:35:08 +0300

    Add UsersController and create user endpoint. (#18)

---
+++ Etos.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Etos.Domain.Entities;
+using Etos.Domain.Repositories;
+
+namespace Etos.Infrastructure.Repositories;
+
+public sealed class UserRepository(ApplicationDbContext dbContext) : IUserRepository
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    public void Add(User user)
+    {
+        _dbContext.Set<User>().Add(user);
+    }
+}
---
+++ Etos.Presentation.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Etos.Application.Users.Commands.CreateUser;
+using Etos.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Etos.Presentation.WebApi.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class UsersController(ISender Sender) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        [FromBody] CreateUserCommand command,
+        CancellationToken cancellationToken)
+    {
+        UserId userId = await Sender.Send(command, cancellationToken);
+        return Ok(userId);
+    }
+}
