bb7c1f2a04d6572dc8b1d5a1a3b4ca1f1e28e1a4
noctvrno <feed.faceit@gmail.com>
2024-08-13 23:01:50 +0300

    Consolidate Requests, Responses and Handlers into corresponding use cases. (Drive-by)

--- Etos.Application/Activities/Queries/GetAllTrainingActivities/ActivitiesResponse.cs
+++
@@ -1,5 +0,0 @@
-﻿using Etos.Domain.Primitives;
-
-namespace Etos.Application.Activities.Queries.GetAllTrainingActivities;
-
-public sealed record ActivitiesResponse(IEnumerable<IActivity> Activities);
