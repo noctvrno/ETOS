58e607562c8bdd971e197e9b7a64ac66afc89a0c
noctvrno <feed.faceit@gmail.com>
2024-08-13 23:01:50 +0300

    Consolidate Requests, Responses and Handlers into corresponding use cases. (Drive-by)

---
+++ Etos.Application/Users/Commands/CreateTraining.cs
@@ -0,0 +1,40 @@
+﻿using Etos.Application.Abstractions;
+using Etos.Domain.Entities;
+using Etos.Domain.Repositories;
+
+namespace Etos.Application.Users.Commands;
+
+public static class CreateTraining
+{
+    public sealed record Request(string Title, UserId AssigneeId) : ICommand;
+
+    public sealed class Handler : ICommandHandler<Request>
+    {
+        private readonly ITrainingActivityRepository _trainingRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Handler(
+            ITrainingActivityRepository trainingRepository,
+            IUserRepository userRepository,
+            IUnitOfWork unitOfWork
+        )
+        {
+            _trainingRepository = trainingRepository;
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task Handle(Request request, CancellationToken cancellationToken)
+        {
+            User user = _userRepository.GetById(request.AssigneeId);
+            var trainingActivity = TrainingActivity.Create(request.Title, user); // TODO: Consider having multiple users which can be assigned after creation.
+
+            _trainingRepository.Add(trainingActivity);
+
+            _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Etos.Application/Users/Commands/CreateUser.cs
@@ -0,0 +1,33 @@
+﻿using Etos.Application.Abstractions;
+using Etos.Domain.Entities;
+using Etos.Domain.Repositories;
+
+namespace Etos.Application.Users.Commands;
+
+public static class CreateUser
+{
+    public sealed record Request(string FirstName, string LastName, string Email) : ICommand<UserId>;
+
+    public sealed class Handler : ICommandHandler<Request, UserId>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Handler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<UserId> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var user = User.Create(request.FirstName, request.LastName, request.Email);
+
+            _userRepository.Add(user);
+
+            _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Task.FromResult(user.Id);
+        }
+    }
+}
