d6a22e42046d9f3a38fcfaf093c3dcd9dba26aed
noctvrno <feed.faceit@gmail.com>
2024-11-09 21:13:41 +0200

    Modify root path to return a Home component which replaces the ActivityFeed. (Drive-by)

---
+++ Etos.Presentation.Client/src/components/activity/Home.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+export default function Home() {
+    return (
+            <Typography
+                variant="h4"
+                sx={{
+                    margin: 4,
+                    marginLeft: 2
+                }}>
+                Home
+            </Typography>
+    );
+}
