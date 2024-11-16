dd507354d8bcdc46b435918c8e626465ca8098dc
noctvrno <feed.faceit@gmail.com>
2024-11-16 22:34:42 +0200

    Qualify Custom Schema Ids as the types' full name. (Drive-by)

---
+++ Etos.Presentation.WebApi/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Etos.Presentation.WebApi.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToSwaggerCompliantSchemaName(this string? nonCompliantName)
+    {
+        return nonCompliantName?.Replace('+', '.') ?? Guid.NewGuid().ToString();
+    }
+}
